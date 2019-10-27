113749e32075f67ba46258f1c5e9aa3b990ca2cf
Biankha <54651808+Biankha@users.noreply.github.com>
2019-10-27 12:06:03 -0400

    o

---
+++ Platformer Gme Project/Assets/Scripts/AudioController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    Animator anim;
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetInteger("State", 0);
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))//running
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D)) //back to idle
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))//running
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A)) //back to idle
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))//running
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow)) //back to idle
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))//running
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow)) //back to idle
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))//jump
+        {
+            anim.SetInteger("State", 3);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W)) //back to idle
+        {
+            anim.SetInteger("State", 0);
+        }
+    }
+
+    
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if (Input.GetKeyUp(KeyCode.W)) //back to idle
+            {
+            anim.SetInteger("State", 0);
+            }
+        }
+    }
+}
+
--- Platformer Gme Project/Assets/Scripts/PlayerScript.cs
+++ Platformer Gme Project/Assets/Scripts/PlayerScript.cs
@@ -7,38 +7,67 @@ public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
     public float speed;
-    public Text score;
+    public Text scoreText;
+    public Text winText;
+    public Text livesText;
+    public Text levelText;
     private int scoreValue = 0;
-    
-    // Start is called before the first frame update
+    private int lives;
+    public GameObject other;
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    Animator anim;
+    private bool facingRight = true;
+
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = "Score: " + scoreValue.ToString();
+        scoreText.text = "Score: " + scoreValue.ToString();
+        winText.text = "";
+        levelText.text = "";
+        livesText.text = "Lives: 3";
+        lives = 3;
+        musicSource.clip = musicClipTwo;
+        musicSource.Play();
+        SetWinText();
+        SetScoreText();
+        anim = GetComponent<Animator>();
+
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         float hozMovement = Input.GetAxis("Horizontal");
         float verMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, verMovement * speed));
 
+        if (facingRight == false && hozMovement > 0) //for facing both ways
+        {
+            Flip();
+        }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
+
         if (Input.GetKey("escape"))
             Application.Quit();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision) //for making coins disappear
     {
         if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = "Score: " + scoreValue.ToString();
+            scoreText.text = "Score: " + scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+            SetWinText();
+            SetScoreText(); 
         }
     }
 
-    private void OnCollisionStay2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision) //for jumping
     {
         if (collision.collider.tag == "Ground")
         {
@@ -49,4 +78,60 @@ public class PlayerScript : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D other) //to make enemies disappear
+    {
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetLivesText();
+            SetScoreText();
+        }
+    }
+
+    void SetScoreText()
+    {
+        if (scoreValue == 4)
+        {
+            livesText.text = "Lives: 3";
+            lives = 3;
+            transform.position = new Vector2(42.27f, -0.15f);
+            levelText.text = "Level 2";
+        }
+
+        else if (scoreValue > 5)
+        {
+            levelText.text = "";
+        }
+    }
+
+    void SetWinText()
+    {
+        winText.text = "";
+        if (scoreValue == 8)
+        {
+            winText.text = "You win! \n Game created by Biankha Hughes!";
+            musicSource.clip = musicClipOne;
+            musicSource.Play();
+        }
+    }
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives <= 0)
+        {
+            winText.text = "You Lose! \n Game created by Biankha Hughes.";
+        }
+    }
+
+    void Flip() //flips character
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
 }
+
+
